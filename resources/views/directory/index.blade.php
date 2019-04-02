9b82dd9cf3f8b55d23797b5325e49d53a02bd553
Pang Chong Xian <chongxian0117@gmail.com>
2019-04-02 12:22:45 +0800

    Upload Photo

--- resources/views/directory/index.blade.php
+++ resources/views/directory/index.blade.php
@@ -63,7 +63,13 @@ use App\Category;
     <div class="card-columns" style="margin: 20px 20px 20px 20px">
         @foreach ($tenants as $i => $tenant)
             <div class="card shadow">
-                <img class="card-img-top" src="https://www.aucklandairport.co.nz/-/media/Images/Traveller/Retail/Stores/Store-Main-Images/Adidas.ashx?mw=1300&hash=A76FE9990B9D3A83358256755B0823BDA9727D55" alt="Card image cap">
+                @if(Storage::disk('public')->exists('tenant/'.$tenant->id.'.jpg'))
+                <img src="/storage/tenant/{{$tenant->id}}.jpg"
+                width="240" alt= {{ $tenant->name}}>
+                @else
+                <img src="http://www.totalbattery.com/wp-content/uploads/2017/04/Under_construction-300x300.png"
+                width="240">
+                @endif
                 <div class="card-body">
                     <h5 class="card-title"> {{ $tenant->name }} </h5>
                     <h6 class="card-subtitle mb-2 text-muted"> {{ $tenant->floor->code."-".$tenant->zone->code."-".$tenant->lot_number }}</h6>
