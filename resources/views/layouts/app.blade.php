1153646093c028242b821622d1d32d4f10a29cb7
Pang Chong Xian <chongxian0117@gmail.com>
2019-04-02 13:22:17 +0800

    Change the comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,7 +109,7 @@
                    </ul>
                    </li>
 
-                   <!-- Tenant Side -->
+                   <!-- Zone Side -->
                    <li class="nav-item dropdown">
                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                        {{ 'Zone' }} <span class="caret"></span>
